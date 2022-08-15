015134dea049054a50a45a6f3e1f78cac9a46e3b
felixbuhler <felixbuhler@users.noreply.github.com>
2022-08-15 13:09:58 +0000

    deploy: 56c455a5cd1cf5648d369c71c5bbd61a62ab33e1

--- js/site.js
+++ js/site.js
@@ -1,10 +1,10 @@
 Marquee3k.init()
 
 // Hide Description
-
+$('project-info').hide(500)
 $('.checkcheck').click(function () {
 
-    this.checked ? $('project-info').hide(500) : $('project-info').show(500);
+    this.checked ? $('project-info').show(500) : $('project-info').hide(500);
 
     this.checked ? $("body").get(0).style.setProperty("--color-switch", "var(--color-accent)") : $("body").get(0).style.setProperty("--color-switch", "var(--color-red)")
 });
@@ -21,10 +21,3 @@ const sortable = new Draggable.Sortable(document.querySelectorAll('projects'), {
     plugins: [Draggable.Plugins.SwapAnimation]
 });
 
-// Switch Follow
-
-$(document).mousemove(function (e) {
-    $('switch').offset({
-        left: e.pageX - 30
-    });
-});
