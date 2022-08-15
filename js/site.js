8a8ee429ec6c4a75d1f9711e7c8a38893e843df8
felixbuhler <felixbuhler@users.noreply.github.com>
2022-08-15 10:02:03 +0000

    deploy: 5988fd2838e21434f96e8cb053853e72da21ca17

--- js/site.js
+++ js/site.js
@@ -1,5 +1,18 @@
 Marquee3k.init()
 
+
+$('project-info').hide(500)
 $('.checkcheck').click(function() {
-    this.checked ? $('project-info').hide(500) : $('project-info').show(500); //time for show
+    
+    this.checked ? $('project-info').show(500) : $('project-info').hide(500); //time for show
 });
+
+// Draggable
+
+const sortable = new Draggable.Sortable(document.querySelectorAll('projects'), {
+    draggable: 'project',
+    swapAnimation: {
+      duration: 200,
+      easingFunction: 'ease-in-out'    },
+    plugins: [Draggable.Plugins.SwapAnimation]
+  });
