33a0a5305929f50e4f04248d7c4b8e100911fd43
felixbuhler <felix.buhler@me.com>
2022-08-15 13:53:51 +0200

    Update
    Update

--- themes/felixbuhler/static/js/site.js
+++ themes/felixbuhler/static/js/site.js
@@ -1,10 +1,12 @@
 Marquee3k.init()
 
+// Hide Description
 
-$('project-info').hide(500)
-$('.checkcheck').click(function() {
-    
-    this.checked ? $('project-info').show(500) : $('project-info').hide(500); //time for show
+$('.checkcheck').click(function () {
+
+    this.checked ? $('project-info').hide(500) : $('project-info').show(500);
+
+    this.checked ? $("body").get(0).style.setProperty("--color-switch", "var(--color-accent)") : $("body").get(0).style.setProperty("--color-switch", "var(--color-red)")
 });
 
 // Draggable
@@ -12,7 +14,17 @@ $('.checkcheck').click(function() {
 const sortable = new Draggable.Sortable(document.querySelectorAll('projects'), {
     draggable: 'project',
     swapAnimation: {
-      duration: 200,
-      easingFunction: 'ease-in-out'    },
+        duration: 200,
+        easingFunction: 'ease-in-out',
+        horizontal: true
+    },
     plugins: [Draggable.Plugins.SwapAnimation]
-  });
+});
+
+// Switch Follow
+
+$(document).mousemove(function (e) {
+    $('switch').offset({
+        left: e.pageX - 30
+    });
+});
