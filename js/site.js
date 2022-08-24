76321f7d6150e8848178be7913404ee8ff651bb2
felixbuhler <felixbuhler@users.noreply.github.com>
2022-08-24 14:10:14 +0000

    deploy: 0cde53689adb52e98382bb7232fb5de540018a6e

--- js/site.js
+++ js/site.js
@@ -66,19 +66,19 @@ const minsHand = document.querySelector('.minute-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
-    const now = new Date();
+  const now = new Date();
 
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
-    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-    const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
@@ -87,3 +87,10 @@ setDate();
 
 
 
+// A-Frame Mobile UI
+
+
+
+$(document).ready(function () {
+  $('.a-modal').appendTo('body');
+});
