e5a97edc60861545f004a12abc10aa3b247a3d02
felixbuhler <felix.buhler@me.com>
2022-11-25 11:19:18 +0100

    CMP

--- themes/felixbuhler/static/js/config.js
+++ themes/felixbuhler/static/js/config.js
@@ -89,78 +89,7 @@ var klaroConfig = {
     translations that can be overwritten:
     https://github.com/KIProtect/klaro/tree/master/src/translations
     */
-    translations: {
-        /*
-        The `zz` key contains default translations that will be used as fallback values.
-            This can e.g. be useful for defining a fallback privacy policy URL.
-        */
-        zz: {
-            privacyPolicyUrl: '/privacy',
-
-        }
-        de: {
-            /*
-            You can specify a language-specific link to your privacy policy here.
-            */
-            privacyPolicyUrl: '/datenschutz',
-            consentNotice: {
-                description: 'Dieser Text wird in der Einwilligungsbox erscheinen.',
-            },
-            consentModal: {
-                description:
-                    'Hier können Sie einsehen und anpassen, welche Information wir über Sie ' +
-                    'sammeln. Einträge die als "Beispiel" gekennzeichnet sind dienen lediglich ' +
-                    'zu Demonstrationszwecken und werden nicht wirklich verwendet.',
-            },
-            /*
-            You should also define translations for every purpose you define in the
-            'services' section. You can define a title and an (optional) description.
-            */
-            purposes: {
-                analytics: {
-                    title: 'Besucher-Statistiken'
-                },
-                security: {
-                    title: 'Sicherheit'
-                },
-                livechat: {
-                    title: 'Live Chat'
-                },
-                advertising: {
-                    title: 'Anzeigen von Werbung'
-                },
-                styling: {
-                    title: 'Styling'
-                },
-            },
-        },
-        en: {
-            privacyPolicyUrl: '/privacy',
-            consentModal: {
-                description:
-                    'Here you can see and customize the information that we collect about you. ' +
-                    'Entries marked as "Example" are just for demonstration purposes and are not ' +
-                    'really used on this website.',
-            },
-            purposes: {
-                analytics: {
-                    title: 'Analytics'
-                },
-                security: {
-                    title: 'Security'
-                },
-                livechat: {
-                    title: 'Livechat'
-                },
-                advertising: {
-                    title: 'Advertising'
-                },
-                styling: {
-                    title: 'Styling'
-                },
-            },
-        },
-    },
+    
 
     /*
     Here you specify the third-party services that Klaro will manage for you.
